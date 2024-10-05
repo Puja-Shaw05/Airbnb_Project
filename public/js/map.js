00f029999ac9eb11722b14a8d8a5076743083a03
Puja Shaw <pujakm9163@gmail.com>
2024-10-05 20:53:12 +0530

    Files Updated

--- public/js/map.js
+++ public/js/map.js
@@ -1,20 +1,11 @@
 mapboxgl.accessToken = mapToken;
-
-
 const map = new mapboxgl.Map({
-  container: 'map',
-  // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: listing.geometry.coordinates,
-    zoom: 9
+  container: "map", // container ID
+  center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 6, // starting zoom
 });
 
-console.log(map);
-
-
-// Create a default Marker and add it to the map.
 const marker = new mapboxgl.Marker({ color: 'red'})
  .setLngLat(listing.geometry.coordinates)
- .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<h4>${listing.location}</h4><P>Exact location will be provided after booking!</p>`))
+ .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<P><b>${listing.location}, ${listing.country}</b></p><p>Exact location will be provided after booking!</p>`))
  .addTo(map);
-
